8102c3bd9fe6f1b02924e6e8796ba5d6b788eedf
Lais <laisfsgomes@gmail.com>
2021-12-09 23:48:08 -0300

    cabo

--- afd.py
+++ afd.py
@@ -1,48 +1,49 @@
-# Programa que lê um AFD com o alfabeto {a, b}
-#Autor: Lais de Fátima Sousa Gomes
 
-#recebe uma lista e uma string e retorna TRUE caso esse valor não exista na lista
-def naoExiste(lista, str_value):
-  saida = True
-  for i in lista:
-    if i == str_value:
-      saida = False
-  return saida
-
+#                       TEORIA DOS AUTOMATOS E LINGUAGENS FORMAIS
+#        Programaque simula o processamento de qualquer AFD sob o alfabeto {a, b}
+#
+#                             Autor: Lais de Fátima Sousa Gomes
+#                                     Matrícula: 504405
+#
+#
+#___________________________________________________________________________________
 
 #ler todos os estados e retorna uma lista contendo-os
 def le_estados(Q):
   est_temp = ''
   while est_temp != 'vazio':
     est_temp = input('digite o nome do estado - para finalizar, escreva o estado vazio como "vazio": ')
-    if naoExiste(Q, est_temp):
+    if (est_temp in Q) == False:
       Q.append(est_temp)
-    else:
+    elif (est_temp in Q) == True:
       print("Estado já presente na lista. Digite outro!")
   return Q
-
 
 
 # Dentre o conjunto de estados Q, essa função define qual é o estado inicial
 def estado_inicial(Q):
   q0 = ''
   while (q0 in Q)==False:
-    q0 = input("dos estados em Q, qual é o estado inicial? ")
+    q0 = input("Dos estados em Q, qual é o estado inicial? ")
+    if (q0 in Q)== False:
+      print("Digite um estado presente em Q. Tente novamente!")
   return q0
 
-
-
+# define o estados finais
 def estados_finais(Q):
   F = []
   val = ''
      
   while val != 'fim':
     val = input('digite o nome do estado final - para finalizar, escreva "fim": ')
-    if val != 'fim':
-      F.append(val)
+    
+    if (val in Q) or val == 'fim':
+      if val != 'fim':
+        F.append(val)
+    elif (val in Q) == False:
+      print("Estado digitado não está presente em Q, tente outro")
+
   return F
-
-
 
 
 def Delta(Q):
@@ -50,10 +51,8 @@ def Delta(Q):
   func_trans = {}
   for i in Q:
     if i != 'vazio':
-      concA = 'valor para o estado '+ i + ' em a'
-      valA = input(concA)
-      concB = 'valor para o estado '+ i + ' em b'
-      valB = input(concB)
+      valA = input('valor para o estado '+ i + ' em a: ')
+      valB = input('valor para o estado '+ i + ' em b: ')
       func_trans[(i, 'a')] = valA
       func_trans[(i, 'b')] = valB
   return func_trans
@@ -71,17 +70,27 @@ def afd(delta, q0, F, palavra):
 
 # ________________________________________________________________________________________________________
 
+#lendo o conjunto de estados
 Q = le_estados([])
-qtd_estados  = len(Q)
 print(Q)
-
+print('\n')
+#definindo os estados iniciais e finais
 q0 = estado_inicial(Q)
 print(q0)
-
 F = estados_finais(Q)
 print(F)
+print('\n')
 
+#lendo a função de transição de estados
 func_trans = Delta(Q)
-palavra = input('palavra que se deseja processar: ')
+print('funcao de transiçao')
+print(func_trans)
+print("--")
 
-print(afd(func_trans, q0, F, palavra))
+#Testando a palavra no AFD:
+palavra = ''
+while palavra != "sair":
+  print("\nPara terminar, digite 'sair'")
+  palavra = input('palavra que se deseja processar: ')
+  if palavra != 'sair':
+    print(afd(func_trans, q0, F, palavra))
